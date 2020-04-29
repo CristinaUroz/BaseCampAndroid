3410f244032e6835bf435aed3901700071f42fef
Cristina Uroz Grané <curoz@visual-engin.com>
2020-04-29 17:28:40 +0200

    Initial commit

---
+++ app/src/main/java/com/basecamp/android/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.basecamp.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
